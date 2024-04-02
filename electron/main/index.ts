8a15c72d289983dbc629bf52e592c96b1d2e9c48
axinger <liu_weixing@qq.com>
2024-04-02 15:12:45 +0800

    1

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,6 +1,6 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
-import { release } from 'node:os'
-import { join } from 'node:path'
+import {app, BrowserWindow, shell, ipcMain} from 'electron'
+import {release} from 'node:os'
+import {join} from 'node:path'
 
 // The built directory structure
 //
@@ -15,8 +15,8 @@ import { join } from 'node:path'
 process.env.DIST_ELECTRON = join(__dirname, '..')
 process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
 process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? join(process.env.DIST_ELECTRON, '../public')
-  : process.env.DIST
+    ? join(process.env.DIST_ELECTRON, '../public')
+    : process.env.DIST
 
 // Disable GPU Acceleration for Windows 7
 if (release().startsWith('6.1')) app.disableHardwareAcceleration()
@@ -25,8 +25,8 @@ if (release().startsWith('6.1')) app.disableHardwareAcceleration()
 if (process.platform === 'win32') app.setAppUserModelId(app.getName())
 
 if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(0)
+    app.quit()
+    process.exit(0)
 }
 
 // Remove electron security warnings
@@ -39,79 +39,99 @@ let win: BrowserWindow | null = null
 const preload = join(__dirname, '../preload/index.js')
 const url = process.env.VITE_DEV_SERVER_URL
 const indexHtml = join(process.env.DIST, 'index.html')
+import {Menu} from 'electron'
+
+const appUrl = import.meta.env.VITE_APP_URL
+
 
 async function createWindow() {
-  win = new BrowserWindow({
-    title: '中车数字平台',
-    icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
-    webPreferences: {
-      preload,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  })
+    Menu.setApplicationMenu(null)
+    win = new BrowserWindow({
+        title: '中车数字平台',
+        icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
+        // width: 1024,
+        // height: 768,
+        // minWidth: 1024,
+        // minHeight: 768,
+        fullscreen: true, // 全屏
+        // simpleFullscreen:true,
+        webPreferences: {
+            preload,
+            // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+            // Consider using contextBridge.exposeInMainWorld
+            // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
+    win.maximize();
 
-  if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
-    win.loadURL(url)
-    // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
-  } else {
-    win.loadFile(indexHtml)
-  }
+    // if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
+    //   win.loadURL(url)
+    //   // Open devTool if the app is not packaged
+    //   win.webContents.openDevTools()
+    // } else {
+    //   win.loadFile(indexHtml)
+    // }
+    // win.webContents.openDevTools()
+    // win.loadURL('http://localhost:12000')
+    await win.loadURL(appUrl)
+    // Test actively push message to the Electron-Renderer
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', new Date().toLocaleString())
+    })
 
-  // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
-  })
-
-  // Make all links open with the browser, not with the application
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
-  })
-  // win.webContents.on('will-navigate', (event, url) => { }) #344
+    // Make all links open with the browser, not with the application
+    win.webContents.setWindowOpenHandler(({url}) => {
+        if (url.startsWith('https:')) shell.openExternal(url)
+        return {action: 'deny'}
+    })
+    // win.webContents.on('will-navigate', (event, url) => { }) #344
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  win = null
-  if (process.platform !== 'darwin') app.quit()
+    win = null
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('second-instance', () => {
-  if (win) {
-    // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore()
-    win.focus()
-  }
+    if (win) {
+        // Focus on the main window if the user tried to open another
+        if (win.isMinimized()) win.restore()
+        win.focus()
+    }
 })
 
 app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
-  if (allWindows.length) {
-    allWindows[0].focus()
-  } else {
-    createWindow()
-  }
+    const allWindows = BrowserWindow.getAllWindows()
+    if (allWindows.length) {
+        allWindows[0].focus()
+    } else {
+        createWindow()
+    }
 })
 
 // New window example arg: new windows url
 ipcMain.handle('open-win', (_, arg) => {
-  const childWindow = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  })
+    const childWindow = new BrowserWindow({
+        webPreferences: {
+            preload,
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
 
-  if (process.env.VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${url}#${arg}`)
-  } else {
-    childWindow.loadFile(indexHtml, { hash: arg })
-  }
+    if (process.env.VITE_DEV_SERVER_URL) {
+        childWindow.loadURL(`${url}#${arg}`)
+    } else {
+        childWindow.loadFile(indexHtml, {hash: arg})
+    }
 })
+
+ipcMain.on("window-all-closed", (event, arg) => {
+    console.log("ipcMain收到消息 Received command:", arg);
+    win = null;
+    if (process.platform !== "darwin") app.quit();
+});
