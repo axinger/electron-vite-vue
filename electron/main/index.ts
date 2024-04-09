c6b2eb80cc0a7f5185ca7744d5e30d6ec14b8ace
axinger <liu_weixing@qq.com>
2024-04-09 16:06:22 +0800

    update

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -79,6 +79,8 @@ async function createWindow() {
     // Test actively push message to the Electron-Renderer
     win.webContents.on('did-finish-load', () => {
         win?.webContents.send('main-process-message', new Date().toLocaleString())
+        // 存储数据到localStorage
+        win.webContents.executeJavaScript(`localStorage.setItem('loginType', 'pc')`);
     })
 
     // Make all links open with the browser, not with the application
